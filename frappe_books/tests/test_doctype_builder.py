46df6dbb8b3fcf81f54d8a83d6551d19b3f80761
Mihir Kandoi <kandoimihir@gmail.com>
2026-09-06 10:10:30 +0530

    test: generator indexes ledger lookup fields

--- frappe_books/tests/test_doctype_builder.py
+++ frappe_books/tests/test_doctype_builder.py
@@ -2,7 +2,7 @@
 
 from frappe.tests import UnitTestCase
 
-from frappe_books.dev.doctype_builder import build_naming
+from frappe_books.dev.doctype_builder import build_fields, build_naming
 
 
 class TestDocTypeBuilder(UnitTestCase):
@@ -11,3 +11,18 @@ class TestDocTypeBuilder(UnitTestCase):
 			build_naming({"name": "ItemEnquiry", "naming": "autoincrement"}),
 			{"autoname": "format:{##########}"},
 		)
+
+	def test_ledger_lookup_fields_are_indexed(self):
+		schema = {
+			"name": "AccountingLedgerEntry",
+			"fields": [
+				{"fieldname": "account", "fieldtype": "Link", "target": "Account"},
+				{"fieldname": "referenceName", "fieldtype": "DynamicLink", "references": "referenceType"},
+				{"fieldname": "referenceType", "fieldtype": "Select", "options": []},
+				{"fieldname": "debit", "fieldtype": "Currency"},
+			],
+		}
+		indexed = {
+			field["fieldname"] for field in build_fields(schema, {"Account"}) if field.get("search_index")
+		}
+		self.assertEqual(indexed, {"account", "voucher_no"})
